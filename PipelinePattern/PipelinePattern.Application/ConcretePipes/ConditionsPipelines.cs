ef28b76045a4aa2c9a42b7cf90f1448735414a64
Carlos Pons <karlospn@gmail.com>
2017-08-30 00:47:51 +0200

    Add conditional pipes

---
+++ PipelinePattern/PipelinePattern.Application/ConcretePipes/ConditionsPipelines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using PipelinePattern.Application.Base;
+using PipelinePattern.Application.ConcretePipes.Filters;
+using PipelinePattern.Application.ConcretePipes.Models.Persons;
+using PipelinePattern.Application.ConcretePipes.Transforms;
+
+namespace PipelinePattern.Application.ConcretePipes
+{
+    public class ConditionsPipeline : Pipeline<IEnumerable<PersonsRequest>, IEnumerable<PersonsResponse>>
+    {
+        public ConditionsPipeline()
+        {
+            PipeFuncy = BuildPipe();
+        }
+
+        private Func<IEnumerable<PersonsRequest>, IEnumerable<PersonsResponse>> BuildPipe()
+        {
+            return input => input
+                .Pipe(new FilterByCountry())
+                .Pipe(new SimpleConditionalPipeline<IEnumerable<PersonsRequest>, IEnumerable<PersonsRequest>>(req => true, new FilterByAge()))
+                .Pipe(new CreatePersonsRequest());
+        }
+    }
+}
